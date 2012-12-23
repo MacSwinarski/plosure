e58ab724657dd363b74984bc7db955e54186614d
Maciej Swinarski <mswinarski@atlassian.com>
2012-12-23 23:25:31 +0100

    comments2

--- src/plosure/miasto/sek.clj
+++ src/plosure/miasto/sek.clj
@@ -3,10 +3,12 @@
 (def data [1,2,4,4,5,9,6,4,3,5,6,7,100])
 
 (defn cut1[s]
+  "creates lazy sequence with ascending elements of sequence s"
   (cons (first s) (lazy-seq
     (if (> (first s) (second s)) [] (cut1 (rest s))))))
 
 (defn cut2[s]
+  "creates lazy sequence with ascending elements of sequence s"
   (cons (first s)
         (map second (take-while #(<= (first %) (second %)) (map vector s (rest s))))))
 
